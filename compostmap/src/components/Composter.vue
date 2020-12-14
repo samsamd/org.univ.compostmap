4fbc89022d6afe97c8099383c749c786757c6513
Alex Morel <amorel@codelutin.com>
2020-12-14 13:59:39 +0100

    v-else-if and v-else examples

--- compostmap/src/components/Composter.vue
+++ compostmap/src/components/Composter.vue
@@ -2,6 +2,10 @@
   <h1>Compost {{ id }}</h1>
   <a :href="url">Adresse: {{ adresse }}</a>
   <p v-if="adresse.indexOf('Nantes') > -1">Situé à Nantes !</p>
+  <p v-else-if="adresse.indexOf('Toulouse') > -1">
+    Situé à Toulouse (Chocolatine power)
+  </p>
+  <p v-else>Situé quelque part</p>
   <div class="compost-image">
     <img :src="image" />
   </div>
@@ -13,7 +17,7 @@ export default {
   data() {
     return {
       id: 42,
-      adresse: "28 rue des plantes en pots, 4400 Nantes",
+      adresse: "28 rue des plantes en pots, 4400 Toulouse",
       image: "./assets/img/compost1.png",
       url: "https://vuejs.org/",
     };
