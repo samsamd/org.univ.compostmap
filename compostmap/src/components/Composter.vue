d44ebaca2a490ba23756099b3f0c4c5942d53d05
Alex Morel <amorel@codelutin.com>
2020-12-14 13:55:34 +0100

    v-if example

--- compostmap/src/components/Composter.vue
+++ compostmap/src/components/Composter.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Compost {{ id }}</h1>
   <a :href="url">Adresse: {{ adresse }}</a>
+  <p v-if="adresse.indexOf('Nantes') > -1">Situé à Nantes !</p>
   <div class="compost-image">
     <img :src="image" />
   </div>
@@ -12,7 +13,7 @@ export default {
   data() {
     return {
       id: 42,
-      adresse: "28 rue des plantes en pots",
+      adresse: "28 rue des plantes en pots, 4400 Nantes",
       image: "./assets/img/compost1.png",
       url: "https://vuejs.org/",
     };
