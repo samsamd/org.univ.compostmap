7bafcd70ae8b5f1945af779b20b1a6ed13b9856f
Alex Morel <amorel@codelutin.com>
2020-12-21 16:16:41 +0100

    Bulma columns - size example (responsive)

--- compostmap/src/App.vue
+++ compostmap/src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="columns is-multiline">
-    <p class="column is-half is-success">Moitié de l'écran</p>
-    <p class="column is-one-quarter">Un quart de l'écran</p>
-    <p class="column is-one-quarter">Un autre quart de l'écran</p>
-    <p class="column is-one-third">
-      Un tiers, ce contenu va passer à une nouvelle ligne
+    <p class="column is-one-quarter-desktop is-half-tablet">
+      Quart de l&lsquo;écran desktop, Moitié tablet
+    </p>
+    <p class="column is-one-quarter-desktop is-half-tablet">
+      Quart de l&lsquo;écran desktop, Moitié tablet
+    </p>
+    <p class="column is-one-third-desktop is-one-third-tablet">
+      Tiers de l&lsquo;écran desktop, Tiers tablet
+    </p>
+    <p class="column is-one-quarter is-four-fifths-widescreen">
+      Un quart dans tous les cas sauf sur widescreen
     </p>
   </div>
 </template>
