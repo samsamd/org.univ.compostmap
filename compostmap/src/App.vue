42697218823de889dcec4934cfb4d318bedeb81a
Alex Morel <amorel@codelutin.com>
2020-12-22 15:37:28 +0100

    Exercice 12 - Add responsive MenuView

--- compostmap/src/App.vue
+++ compostmap/src/App.vue
@@ -1,37 +1,47 @@
 <template>
   <div>
-    <p class="columns">
-      <button
-        class="button is-primary"
-        @click="composters.push({ id: nextComposterId++ })"
-      >
-        Add Composter {{ nextComposterId }}
-      </button>
-      <button
-        class="button is-danger is-quarter"
-        @click="composters.splice(0, 1)"
-        v-if="composters.length > 0"
-      >
-        Remove composter {{ composters[0].id }}
-      </button>
-    </p>
-    <div class="columns is-multiline">
-      <Composter
-        v-for="composter in composters"
-        :key="composter.id"
-        class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
-        :composter-id="composter.id"
-      />
+    <!-- MenuView -->
+    <MenuView />
+
+    <!-- Content -->
+    <div class="container">
+      <!-- About Page -->
+      <!-- Composters list -->
+      <p class="columns">
+        <button
+          class="button is-primary"
+          @click="composters.push({ id: nextComposterId++ })"
+        >
+          Add Composter {{ nextComposterId }}
+        </button>
+        <button
+          class="button is-danger is-quarter"
+          @click="composters.splice(0, 1)"
+          v-if="composters.length > 0"
+        >
+          Remove composter {{ composters[0].id }}
+        </button>
+      </p>
+      <div class="columns is-multiline">
+        <Composter
+          v-for="composter in composters"
+          :key="composter.id"
+          class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+          :composter-id="composter.id"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Composter from "./components/Composter.vue";
+import MenuView from "./components/MenuView.vue";
 
 export default {
   name: "App",
   components: {
+    MenuView,
     Composter,
   },
   data() {
@@ -50,6 +60,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5px;
 }
 </style>
---
+++ compostmap/src/components/MenuView.vue
@@ -0,0 +1,133 @@
+<template>
+  <nav class="navbar box" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item animated" href="/">
+        <img
+          class="navbar-brand-img"
+          src="../../public/assets/img/compost1.png"
+        />
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+        :class="{ 'is-active': mobileMenuActive }"
+        @click="mobileMenuActive = !mobileMenuActive"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div class="navbar-menu" :class="{ 'is-active': mobileMenuActive }">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/">
+          Accueil
+        </a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Composteurs
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item" href="/composters">
+              Liste
+            </a>
+            <a class="navbar-item">
+              Carte
+            </a>
+            <hr class="navbar-divider" />
+            <a class="navbar-item" href="/about">
+              A propos
+            </a>
+            <a
+              class="navbar-item"
+              href="https://github.com/alexsedlex/org.univ.compostmap"
+              target="github"
+            >
+              Voir le code source
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item copyright">
+          Created by Univ-Nantes &copy;
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MenuView",
+  data() {
+    return {
+      mobileMenuActive: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  height: 70px;
+}
+.navbar-brand-img {
+  margin-left: 10px;
+  height: 50px;
+  width: 35px;
+}
+.animated {
+  position: relative;
+  z-index: 0;
+  margin-right: 5px;
+  margin-left: 5px;
+  height: 40px;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.animated::before {
+  content: "";
+  position: absolute;
+  z-index: -2;
+  left: -50%;
+  top: -50%;
+  width: 200%;
+  height: 200%;
+  background-color: #399953;
+  background-repeat: no-repeat;
+  background-size: 50% 50%, 50% 50%;
+  background-position: 0 0, 100% 0, 100% 100%, 0 100%;
+  background-image: linear-gradient(#7fff00, #32cd32),
+    linear-gradient(#32cd32, #228b22), linear-gradient(#f4a460, #a0522d),
+    linear-gradient(#a0522d, #800000);
+  animation: rotate 4s linear infinite;
+}
+
+.animated::after {
+  content: "";
+  position: absolute;
+  z-index: -1;
+  left: 6px;
+  top: 6px;
+  width: calc(100% - 12px);
+  height: calc(100% - 12px);
+  background: white;
+  border-radius: 5px;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
