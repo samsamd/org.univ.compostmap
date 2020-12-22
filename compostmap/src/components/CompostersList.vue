b34619f3122956bb855916bb1bead84f10217c60
Alex Morel <amorel@codelutin.com>
2020-12-22 16:20:38 +0100

    Exercice 13

---
+++ compostmap/src/components/CompostersList.vue
@@ -0,0 +1,42 @@
+<template>
+  <p class="columns">
+    <button
+      class="button is-primary"
+      @click="composters.push({ id: nextComposterId++ })"
+    >
+      Add Composter {{ nextComposterId }}
+    </button>
+    <button
+      class="button is-danger is-quarter"
+      @click="composters.splice(0, 1)"
+      v-if="composters.length > 0"
+    >
+      Remove composter {{ composters[0].id }}
+    </button>
+  </p>
+  <div class="columns is-multiline">
+    <Composter
+      v-for="composter in composters"
+      :key="composter.id"
+      class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+      :composter-id="composter.id"
+    />
+  </div>
+</template>
+
+<script>
+import Composter from "./Composter.vue";
+
+export default {
+  name: "CompostersList",
+  components: {
+    Composter,
+  },
+  data() {
+    return {
+      nextComposterId: 1,
+      composters: new Array(),
+    };
+  },
+};
+</script>
