0041120b358d0b933d961570f1f6138d90097662
samuel <samuel.defosse44@gmail.com>
2022-02-28 15:23:09 +0100

    correction exercice 17

--- compostmap/src/main.js
+++ compostmap/src/main.js
@@ -5,3 +5,35 @@ import router from './router'
 createApp(App)
     .use(router)
     .mount('#app')
+
+function asyncFailIfMust(mustFail) {
+     return new Promise(function (resolve, reject) {
+         setTimeout (() => {
+            if(!mustFail) {
+                resolve("succes !");
+            } else {
+                reject('failure !');
+            }
+         }, 3000
+         );
+    })
+}
+console.log("asyncFailIfMust(true)");
+
+asyncFailIfMust(true)
+    .then(result => {
+        console.log("asyncFailIfMust(true)", result);
+    })
+    .catch(failure => {
+        console.error("asyncFailIfMust(true)", failure);
+    })
+
+console.log("asyncFailIfMust(false)");
+
+asyncFailIfMust(false)
+    .then(result => {
+        console.log("asyncFailIfMust(false)", result);
+    })
+    .catch(failure => {
+        console.error("asyncFailIfMust(false)", failure);
+    })
