dabc57d96ed1de3ecff297d8c5e0bf60ec344236
samuel <samuel.defosse44@gmail.com>
2022-02-28 15:43:18 +0100

    correction exercice 18

--- compostmap/src/main.js
+++ compostmap/src/main.js
@@ -6,34 +6,37 @@ createApp(App)
     .use(router)
     .mount('#app')
 
+    someFunction(); 
+
 function asyncFailIfMust(mustFail) {
      return new Promise(function (resolve, reject) {
          setTimeout (() => {
             if(!mustFail) {
-                resolve("succes !");
+                resolve("Resolved");
             } else {
-                reject('failure !');
+                reject('Rejected');
             }
          }, 3000
          );
     })
 }
+
 console.log("asyncFailIfMust(true)");
-
-asyncFailIfMust(true)
-    .then(result => {
-        console.log("asyncFailIfMust(true)", result);
-    })
-    .catch(failure => {
-        console.error("asyncFailIfMust(true)", failure);
-    })
-
 console.log("asyncFailIfMust(false)");
 
-asyncFailIfMust(false)
-    .then(result => {
-        console.log("asyncFailIfMust(false)", result);
-    })
-    .catch(failure => {
-        console.error("asyncFailIfMust(false)", failure);
-    })
+async function someFunction() {
+    try {        
+        var result = await asyncFailIfMust(true);
+        console.log("asyncFailIfMust(true) - succes !", result);
+    } 
+    catch(failure) {
+        console.error("asyncFailIfMust(true) - fail !", failure);
+    }
+    try {       
+        result = await asyncFailIfMust(false);
+        console.log("asyncFailIfMust(false) - succes !", result);
+    } 
+    catch (failure) {
+        console.error("asyncFailIfMust(false) - fail !", failure);
+    }
+}
